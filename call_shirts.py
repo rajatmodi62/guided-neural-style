af62c5c8f6e0fe24eee20cbc55808b7a44372c61
Rajat Modi <rajatmodi@Rajats-MacBook-Pro.local>
2019-08-24 23:36:49 +0530

    a

--- call_shirts.py
+++ call_shirts.py
@@ -4,4 +4,4 @@ for i in range(0,19):
 
 	shirt_path="original_shirts/"+str(i)+".png"
 	print(shirt_path)
-	os.system("python wrapper.py  --shirt_path".format(shirt_path))
+	os.system("python wrapper.py  --shirt_path={}".format(shirt_path))
